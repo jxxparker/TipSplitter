f56ed7bd957387bb5bbd8fe2da1b7c411cb479c0
Jihun Park <jxxparker@gmail.com>
2020-10-02 15:00:25 -0500

    tipchanged

--- Tipsy/Controller/CalculateViewController.swift
+++ Tipsy/Controller/CalculateViewController.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    @IBAction func tipChanged(_sender: UIButton) {
+    var tip = 0.10
+    
+    @IBAction func tipChanged(_  sender: UIButton) {
+        //Deselect all tip buttons via IBOutlets
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
         
+        //Make the button that triggered the IBAction selected.
+        sender.isSelected = true
+        
+        //Get the current title of the button that was pressed.
+        let buttonTitle = sender.currentTitle!
+        
+        //Remove the last character (%) from the title then turn it back into a String.
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        
+        //Turn the String into a Double.
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        
+        //Divide the percent expressed out of 100 into a decimal e.g. 10 becomes 0.1
+        tip = buttonTitleAsANumber / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
@@ -33,7 +53,7 @@ class CalculateViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
